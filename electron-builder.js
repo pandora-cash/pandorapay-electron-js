f6debc825c3f999ea2f7c9f145bbf9f8345c916d
xiangwei22@protonmail.com <xiangwei22@protonmail.com>
2022-09-09 05:28:53 -0500

    builder

---
+++ electron-builder.js
@@ -0,0 +1,73 @@
+const files = [
+    "!release-packager-builds/",
+    "!release-builder-builds/",
+    "!build.sh",
+    "!release-builder.sh",
+    "!release-packager.sh",
+]
+
+const config = require('./config')
+const os = require("os");
+
+module.exports = {
+    directories: {
+        buildResources: 'electron-build',
+        output: 'release-builder-builds',
+    },
+    mac: {
+        category: "public.app-category.utilities",
+        icon: "assets/icons/mac/icon.icns",
+        target: [
+            "pkg",
+            (os.arch() === 'darwin') ? 'dmg' : 'zip',
+        ],
+        files: [
+            ...files,
+            "!helper/pandora-electron-helper-linux-386",
+            "!helper/pandora-electron-helper-linux-amd64",
+            "!helper/pandora-electron-helper-windows-386.exe",
+            "!helper/pandora-electron-helper-windows-amd64.exe",
+        ],
+    },
+    linux: {
+        category: 'Utility',
+        icon: "assets/icons/mac/icon.icns",
+        target: [
+            "AppImage",
+            "tar.gz"
+        ],
+        files: [
+            ...files,
+            "!helper/pandora-electron-helper-darwin-amd64",
+            "!helper/pandora-electron-helper-linux-386",
+            "!helper/pandora-electron-helper-windows-386.exe",
+            "!helper/pandora-electron-helper-windows-amd64.exe",
+        ]
+    },
+    win:{
+        publisherName: config.name,
+        target: [
+            "nsis",
+            "portable",
+        ],
+        icon: "assets/icons/win/icon.ico",
+        files: [
+            ...files,
+            "!helper/pandora-electron-helper-darwin-amd64",
+            "!helper/pandora-electron-helper-linux-386",
+            "!helper/pandora-electron-helper-linux-amd64",
+        ],
+    },
+    dmg: {
+        "icon": "assets/icons/mac/icon.icns",
+        "title": `${config.name} installer`
+    },
+    nsis: {
+        "oneClick": false,
+        "perMachine": false,
+        "allowToChangeInstallationDirectory": true,
+        "installerIcon": "assets/icons/win/icon.ico",
+        "createDesktopShortcut": true,
+        "createStartMenuShortcut": true
+    },
+}
---
+++ release-packager.sh
@@ -0,0 +1,33 @@
+echo "release using electron-packager"
+
+./build.sh wasm wallet electron-helper
+
+name="pandora-cash"
+argsWin="--icon=assets/icons/win/icon.ico  --version-string.CompanyName=CE --version-string.FileDescription=CE --version-string.ProductName='${name}'"
+args=" --overwrite ${name} --prune=true --out=release-packager-builds --ignore='^/release-packager-builds' --ignore='^/release-builder-builds' --ignore='^/_build' --ignore='^/helper' --ignore='^/build.sh' --ignore='^/release.sh' --ignore='^/.idea' --ignore='^/assets' "
+argsLinux="--icon=assets/icons/mac/icon.icns "
+
+npx electron-packager ./ $args --platform=linux --arch=x64 $argsLinux
+npx electron-packager ./ $args --platform=win32 --arch=ia32 $argsWin
+npx electron-packager ./ $args --platform=win32 --arch=x64 $argsWin
+npx electron-packager ./ $args --platform=darwin --arch=x64 $argsLinux
+
+a="./release-builds/${name}-darwin-x64"
+mkdir -p ${a}/${name}.app/Contents/Resources/app/helper
+cp ./helper/pandora-electron-helper-darwin-amd64 ${a}/${name}.app/Contents/Resources/app/helper/
+zip -r $a.zip $a
+
+a="./release-builds/${name}-linux-x64"
+mkdir -p ${a}/resources/app/helper
+cp ./helper/pandora-electron-helper-linux-amd64 ${a}/resources/app/helper/
+zip -r $a.zip $a
+
+a="./release-builds/${name}-win32-ia32"
+mkdir -p ${a}/resources/app/helper
+cp ./helper/pandora-electron-helper-windows-386.exe ${a}/resources/app/helper/
+zip -r $a.zip $a
+
+a="./release-builds/${name}-win32-x64"
+mkdir -p ${a}/resources/app/helper
+cp ./helper/pandora-electron-helper-windows-amd64.exe ${a}/resources/app/helper/
+zip -r $a.zip $a
